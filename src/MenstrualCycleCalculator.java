0a52f699d3bd388ad2aeec5c80331eca462fbb20
mohbaba <babamu09@gmail.com>
2024-01-29 08:58:11 +0100

    renewed access

--- src/MenstrualCycleCalculator.java
+++ src/MenstrualCycleCalculator.java
@@ -1,9 +1,6 @@
 import java.time.LocalDate;
 import java.util.Scanner;
 
-
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class MenstrualCycleCalculator {
     private int averagePeriodCycle;
     private  int lastPeriodDate;
